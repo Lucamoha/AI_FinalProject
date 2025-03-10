218d1e6dc9be9d2cd405ce4c9c840a0aa540f072
VanPhuHien <phien0689@gmail.com>
2025-03-10 22:06:35 +0700

    Update food.py
    Update food

--- Pacman_Game/objects/food.py
+++ Pacman_Game/objects/food.py
@@ -3,5 +3,20 @@ import pygame as pg
 from constants import *
 
 class Food:
-    def __init__(self):
-        pass
+    def __init__(self, row, col, size=BLOCK_SIZE, color=WHITE):
+        self.size = size
+        self.color = color
+
+        # Tọa độ của thức ăn 
+        self.x = col * SIZE_WALL + MARGIN["LEFT"] + SIZE_WALL // 2
+        self.y = row * SIZE_WALL + MARGIN["TOP"] + SIZE_WALL // 2
+
+        self.rect = pg.Rect(self.x - self.size // 2, self.y - self.size // 2, self.size, self.size) # Ranh giới va chạm của thưc ăn
+
+    # Vẽ thức ăn
+    def draw(self, screen):
+        pg.draw.circle(screen, self.color, (self.x, self.y), self.size)
+
+    # Kiểm tra xem thức ăn đã bị ăn chưa
+    def check_food(self, player_rect): 
+        return self.rect.colliderect(player_rect)
