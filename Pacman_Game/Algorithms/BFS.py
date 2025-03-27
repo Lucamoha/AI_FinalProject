90e587f099aa617c73f06d8be72cd7f769ea2870
Lucamoha <trantrieuduong2411@gmail.com>
2025-03-27 10:22:55 +0700

    Update BFS

--- Pacman_Game/Algorithms/BFS.py
+++ Pacman_Game/Algorithms/BFS.py
@@ -1,16 +1,11 @@
 from constants import *
 from collections import deque
+from Algorithms.utils import distance_to_ghost
 
 def BFS(start: tuple, food_positions: list, ghost_positions: list, wall_positions: list):
     """BFS giúp Pacman tìm đường đi nhưng tránh quái."""
     queue = deque([(start, [])])  # Lưu trữ (vị trí hiện tại, đường đi)
     visited = {start}
-
-    def distance_to_ghost(pos):
-        """Tính khoảng cách Manhattan đến quái gần nhất."""
-        if not ghost_positions:
-            return float('inf')
-        return min(abs(pos[0] - g[0]) + abs(pos[1] - g[1]) for g in ghost_positions)
 
     while queue:
         (r, c), path = queue.popleft()
@@ -22,7 +17,7 @@ def BFS(start: tuple, food_positions: list, ghost_positions: list, wall_position
         for dr, dc in MOVES:
             nr, nc = r + dr, c + dc
             if (nr, nc) not in visited and (nr, nc) not in wall_positions and (nr, nc) not in ghost_positions:
-                next_moves.append(((nr, nc), distance_to_ghost((nr, nc))))
+                next_moves.append(((nr, nc), distance_to_ghost((nr, nc), ghost_positions)))
 
         # Sắp xếp các bước đi ưu tiên tránh xa quái
         next_moves.sort(key=lambda x: x[1], reverse=True)
