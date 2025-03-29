230a0b3a9c97414323d6b556b3b46c368f15d36a
VanPhuHien <phien0689@gmail.com>
2025-03-29 18:13:02 +0700

    Update Ghost_move.py
    add random_move and fix A_star

--- Pacman_Game/Algorithms/Ghost_move.py
+++ Pacman_Game/Algorithms/Ghost_move.py
@@ -1,51 +1,62 @@
 from constants import *
 import heapq
+import random
 from Algorithms.utils import manhattan
 
-def A_star(start: tuple, pacman_positions: list, wall_positions: list):
-    """A* giúp ghost tìm đường đi ngắn nhất đến Pacman gần nhất."""
-    if not pacman_positions:
+def A_star(start: tuple, pacman_position: tuple, wall_positions: list):
+    """A* giúp ghost tìm đường đi ngắn nhất đến Pacman"""
+    if not pacman_position:
         return []
 
-    # Hàng đợi ưu tiên lưu trữ (f(n), g(n), vị trí hiện tại, đường đi, mục tiêu Pacman)
+    # Hàng đợi ưu tiên lưu trữ (f(n), g(n), vị trí hiện tại, đường đi)
     pq = []
     visited = {start}
-    
-    def find_nearest_pacman(pos):
-        if not pacman_positions:
-            return None
-        return min(pacman_positions, key=lambda p: manhattan(pos, p))
 
-    # Chọn Pacman gần nhất từ vị trí start
-    target = find_nearest_pacman(start)
-    if target is None:
-        return []
+    h = manhattan(start, pacman_position)
+    heapq.heappush(pq, (h, 0, start, []))
 
-    # Thêm vị trí start vào hàng đợi với mục tiêu là Pacman gần nhất
-    h = manhattan(start, target)
-    heapq.heappush(pq, (h, 0, start, [], target))
-    
     while pq:
         # Lấy nút có f(n) nhỏ nhất
-        f, g, (r, c), path, current_target = heapq.heappop(pq)
-        
-        if (r, c) == current_target:
-            return path + [(r, c)] # Tìm thấy đường đi
-        
+        f, g, (r, c), path = heapq.heappop(pq)
+
+        if (r, c) == pacman_position:
+            return path + [(r, c)]  # Tìm thấy đường đi
+
         # Kiểm tra các bước đi lân cận (lên, xuống, trái, phải)
         for dr, dc in MOVES:
             nr, nc = r + dr, c + dc
             if (nr, nc) not in visited and (nr, nc) not in wall_positions:
                 visited.add((nr, nc))
                 new_g = g + 1  # Chi phí từ start đến vị trí mới
-                
-                # Tìm Pacman gần nhất từ vị trí mới
-                new_target = find_nearest_pacman((nr, nc))
-                if new_target is None:
-                    continue
-                
-                h = manhattan((nr, nc), new_target)  # Ước lượng chi phí 
-                f = new_g + h  # Tổng chi phí 
-                heapq.heappush(pq, (f, new_g, (nr, nc), path + [(nr, nc)], new_target))
-    
+
+                h = manhattan((nr, nc), pacman_position)  # Ước lượng chi phí
+                f = new_g + h  # Tổng chi phí
+                heapq.heappush(pq, (f, new_g, (nr, nc), path + [(nr, nc)]))
+
     return []  # Không tìm thấy đường đi
+
+
+def random_move(start: tuple, wall_positions: list):
+    """Ghost di chuyển ngẫu nhiên theo 4 hướng"""
+    row, col = start 
+
+    # Tạo các hướng di chuyển ngẫu nhiên
+    possible_moves = []
+    for dr, dc in MOVES:
+        new_row = row + dr 
+        new_col = col + dc
+        new_position = (new_row, new_col)
+        possible_moves.append(new_position)
+    random.shuffle(possible_moves)
+
+    # Lọc các ô đi được
+    can_move = []
+    for move in possible_moves:
+        if move not in wall_positions:
+            can_move.append(move) 
+
+    # Nếu có đường đi thì chọn một hướng ngẫu nhiên
+    if can_move:
+        return random.choice(can_move)
+
+    return []
